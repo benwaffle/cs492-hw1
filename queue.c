c194eab13e3ffd7c521862971caad2ffdf9866e4
Ben Iofel <iofelben@gmail.com>
2017-03-01 14:03:05 -0500

    implement unlimited queue

--- queue.c
+++ queue.c
@@ -1,13 +1,27 @@
 #include <assert.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include "queue.h"
 
 queue *queue_new(int size) {
-    queue *q = malloc(sizeof(queue) + sizeof(product) * size);
+    bool unlimited = false;
+    if (size == 0) {
+        unlimited = true;
+        size = 10;
+    }
+
+    queue *q = calloc(1, sizeof(queue));
+    q->unlimited = unlimited;
     q->size = size;
     q->length = 0;
+    q->data = calloc(size, sizeof(product));
     return q;
+}
+
+void queue_free(queue *q) {
+    free(q->data);
+    free(q);
 }
 
 product queue_peek(queue *q) {
@@ -15,7 +29,15 @@ product queue_peek(queue *q) {
 }
 
 void queue_push(queue *q, product p) {
-    assert(!queue_full(q));
+    if (q->unlimited) {
+        if (q->length == q->size) { // queue is full, increase memory
+            q->size *= 2;
+            q->data = realloc(q->data, q->size * sizeof(product));
+        }
+    } else {
+        assert(!queue_full(q));
+    }
+
     memmove(&q->data[1], &q->data[0], sizeof(product) * q->length);
     q->data[0] = p;
     q->length++;
@@ -27,7 +49,10 @@ product queue_pop(queue *q) {
 }
 
 bool queue_full(queue *q) {
-    return q->length == q->size;
+    if (q->unlimited)
+        return false;
+    else
+        return q->length == q->size;
 }
 
 bool queue_empty(queue *q) {
@@ -54,7 +79,17 @@ int main() {
     queue_push(q, (product){.productid = 4});
 
     assert(queue_full(q));
-    
-    free(q);
+
+    queue_free(q);
+
+    q = queue_new(UNLIMITED);
+    for (int i=0; i<1000; ++i)
+        queue_push(q, (product){.productid = i});
+    assert(!queue_full(q));
+    assert(queue_peek(q).productid == 0);
+    for (int i=0; i<1000; ++i)
+        assert(queue_pop(q).productid == i);
+    assert(queue_empty(q));
+    queue_free(q);
 }
 #endif
--- queue.h
+++ queue.h
@@ -3,13 +3,17 @@
 #include <stdbool.h>
 #include "product.h"
 
+#define UNLIMITED 0
+
 typedef struct {
+    bool unlimited;
     int size; // capacity
     int length;
-    product data[];
+    product *data;
 } queue;
 
 queue *queue_new(int size);
+void queue_free(queue*);
 
 product queue_peek(queue*);
 void queue_push(queue*, product);
